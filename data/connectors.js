575cb292be96e0c9ffe6a8cbbff37cffdd4bbc54
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-01-12 23:33:24 +0100

    Add some indexes and constraints to the mongoose schemas

--- data/connectors.js
+++ data/connectors.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose'),
+let mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
@@ -8,36 +8,37 @@ const mongo = mongoose.connect('mongodb://localhost', {
 });
 
 const MatrixSchema = Schema({
-    title: String,
+    title: { type: String, index: { unique: true } },
     description: String,
     categories: [{ type: Schema.ObjectId, ref: 'Category' }],
     alternatives: [{ type: Schema.ObjectId, ref: 'Alternative' }],
     creationDate: { type: Date, default: Date.now },
 });
-
 const Matrix = mongoose.model('Matrix', MatrixSchema);
 
 const CategorySchema = Schema({
     title: String,
     description: String,
-    sorting: { type: Number, index: { unique: true } },
+    sorting: { type: Number, min: 0 },
     weight: { type: Number, default: 0 },
     entries: [{ type: Schema.ObjectId, ref: 'Entry' }],
     matrix: { type: Schema.ObjectId, ref: 'Matrix' },
     creationDate: { type: Date, default: Date.now },
 });
-
+CategorySchema.index({ matrix: 1, title: 1 }, { unique: true });
+CategorySchema.index({ matrix: 1, sorting: 1 }, { unique: true });
 const Category = mongoose.model('Category', CategorySchema);
 
 const AlternativeSchema = Schema({
     title: String,
     description: String,
-    sorting: { type: Number, index: { unique: true } },
+    sorting: { type: Number, min: 0 },
     entries: [{ type: Schema.ObjectId, ref: 'Entry' }],
     matrix: { type: Schema.ObjectId, ref: 'Matrix' },
     creationDate: { type: Date, default: Date.now },
 });
-
+AlternativeSchema.index({ matrix: 1, title: 1 }, { unique: true });
+AlternativeSchema.index({ matrix: 1, sorting: 1 }, { unique: true });
 const Alternative = mongoose.model('Alternative', AlternativeSchema);
 
 const EntrySchema = Schema({
@@ -47,7 +48,7 @@ const EntrySchema = Schema({
     category: { type: Schema.Types.ObjectId, ref: 'Category' },
     creationDate: { type: Date, default: Date.now },
 });
-
+EntrySchema.index({ alternative: 1, category: 1 }, { unique: true });
 const Entry = mongoose.model('Entry', EntrySchema);
 
 export { Matrix, Category, Alternative, Entry };
